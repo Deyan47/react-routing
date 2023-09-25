2296e757f4b0c5fd0130c19d23207ae06acc8930
Deyan Yonkov <deqnyonkov47@gmail.com>
2023-09-26 00:08:14 +0300

    Demo finished

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+  },
+];
+
 const Products = () => {
   return (
     <>
       <h1>Products</h1>
-      <p>
-        Go to <Link to="/">Home</Link>
-      </p>
+      <ul>
+        {PRODUCTS.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
